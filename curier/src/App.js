00193a995ea6d41b4aa20ecccffce4bc9f32ae45
ZzDamon <damonlepirda@gmail.com>
2021-03-23 21:31:40 +0200

    connected to the hub

--- curier/src/App.js
+++ curier/src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { HubConnection } from 'signalr-client-react';
+import { HubConnectionBuilder } from '@microsoft/signalr';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// let connection = new HubConnection('/chat');
+ 
+// connection.on('NewOrder', data => {
+//     console.log(data);
+// });
+
+//TO SEND ORDER FROM CLIENT :
+// connection.invoke("AddOrder", order).catch(function (err) {
+  // return console.error(err.toString());
+ 
+// connection.start();
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      orders:[],
+      takenOrder:null,
+      connection: new HubConnectionBuilder()
+            .withUrl('http://localhost:5000/hubs/OrdersHub')
+            .withAutomaticReconnect()
+            .build()
+    }
+  }
+
+  componentDidMount(){
+    this.start();
+  }
+
+  start=async ()=> {
+    try {
+        await this.state.connection.start();
+        this.state.connection.on("NewOrder",(order) => this.addNewOrder(order) ) ;
+        console.log("SignalR Connected.");
+
+        this.state.connection.invoke("JoinRoom", "couriers")
+        // .catch(function (err) {
+        //   return console.error(err.toString());})
+        } 
+  
+    catch (err) {
+      console.log(err);     
+    }
+  
+  };
+
+  addNewOrder(order){
+    this.setState(prevState=>
+      {return {
+        orders: [
+          ...prevState.orders,
+          order
+        ]
+      }});
+               
+    console.log(this.state.orders)
+  }
+
+  render(){
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+
+  
+
 }
 
 export default App;
